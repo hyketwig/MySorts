9af54a1ccf5f946283d6ae3654a0c6c44bf7e144
hyketwig <jli31@stuy.edu>
2015-12-23 10:33:15 -0500

    bogo

---
+++ BogoSort.java
@@ -0,0 +1,49 @@
+public class BogoSort {
+
+    import java.util.ArrayList;
+
+
+    //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+    //precond: lo < hi && size > 0
+    //postcond: returns an ArrayList of random integers
+    //          from lo to hi, inclusive
+    public static ArrayList populate( int size, int lo, int hi ) 
+    {
+	ArrayList<Integer> retAL = new ArrayList<Integer>();
+	while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+	}
+	return retAL;
+    }
+
+
+    //randomly rearrange elements of an ArrayList
+    public static void shuffle( ArrayList al ) 
+    {
+	int randomIndex;
+	//setup for traversal fr right to left
+        for( int i = al.size()-1; i > 0; i-- ) {
+	    //pick an index at random
+            randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+            al.set( i, al.set( randomIndex, al.get(i) ) );
+        }
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    // VOID version of bubbleSort
+    // Rearranges elements of input ArrayList
+    // postcondition: data's elements sorted in ascending order
+    public static void bogoSortV( ArrayList<Comparable> data )
+    {
+
+	for (int i = 0; i < data.size(); i++) {
+	    if (data.get(i).compareTo(data.get(i+1)) > 0) {
+		shuffle( data 
+	
+    }//end bogoSortV
+
+}
